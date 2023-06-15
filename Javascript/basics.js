e35ce831862d74ae3dfcfac9e02fea780f071539
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-06-15 17:12:02 +0530

    Update basics.js

--- Javascript/basics.js
+++ Javascript/basics.js
@@ -1,1 +1,10 @@
+/* Equals operator (===) */
+// The === operator is commonly referred to as the strict equality operator. It compares two values and evaluates to true if they are equal.
 
+function isEqual(a, b) {
+   if( a === b){
+       console.log("true"); // prints true if a and b are equal
+   }
+}
+
+module.exports = isEqual; //call the function
