870fef9dab5f8b9f030097329448bff5ecf5bac9
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-06-15 17:24:25 +0530

    Update basics.js

--- Javascript/basics.js
+++ Javascript/basics.js
@@ -13,3 +13,23 @@ module.exports = isEqual; //call the function
 
 /* Not Equals operator (!==) */
 //  !== or the strict inequality operator. This operator will evaluate to true if the two values are not equal.
+
+/* QUESTION:- 
+Given two arguments, first and last, determine which one is greater.
+If first is greater than last, return first!
+If last is greater than first, return last!
+If they are equal, do not return anything.    */
+
+function greater(first, last) {
+    if (first > last){
+        return first;
+    } 
+    else if(last > first){
+        return last;
+    }
+    else{
+            // NOTE: when u don't return anything, The value returned from funtion in this case is 'undefined'.
+    }
+}
+
+module.exports = greater;
