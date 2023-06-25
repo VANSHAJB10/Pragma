3af8a2382a7c264395863cdf191dd4b41be5aafc
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-06-25 21:01:59 +0530

    Create Foundation.js

---
+++ Javascript/Foundation.js
@@ -0,0 +1,5 @@
+Javascript is the language that Browser can understand.
+
+All the Js compilers have to follow the ECMA Script Standard  
+     It providers provides Standards for Browsers to follow so that Js code can be interpreted
+Browser Engine converts Js code to Binary
