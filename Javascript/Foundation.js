f3bd5efa3b74d0b7cc78aa7aaae2ec7036ec910e
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-06-26 23:40:43 +0530

    Update Foundation.js

--- Javascript/Foundation.js
+++ Javascript/Foundation.js
@@ -3,3 +3,5 @@ Javascript is the language that Browser can understand.
 All the Js compilers have to follow the ECMA Script Standard  
      It providers provides Standards for Browsers to follow so that Js code can be interpreted
 Browser Engine converts Js code to Binary
+
+All browsers have their own compilers to interpret Js code in accordance with ECMA Script Standard
